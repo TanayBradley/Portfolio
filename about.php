49808859cca229eee0cbb965a48dc208ca9eafd3
Tanay Bradley <tanaysbradley@gmail.com>
2025-01-09 12:34:04 -0600

    Commit

--- about.php
+++ about.php
@@ -17,11 +17,13 @@
         <div id="aboutTitle">
             <div>Behind The Screen: My Story</div></div>
         <div id="aboutBody">
-            <span>Hey there, I’m Tanay!<br/><br/>I’m a UX Researcher and Strategist passionate about uncovering user needs to drive impactful designs. When I’m not shaping experiences, you can find me working as a recording artist, exploring the outdoors, or diving into a good book.</span>
+            <span>Hey there, I’m Tanay!<br/><br/>I am a UX Researcher, Strategist, and Recording Artist with a love for blending music and design. My Design journey began after receiving my associates in Graphic Design, which laid a foundation and sparked a curiosity about how Design impacts user experiences, and inspired me to create more meaningful interactions. I then decided to attend Full Sail University to pursue a bachelor’s degree in User Experience and graduated in 2024. In the process, I also obtained a certificate in Recording Arts for a deeper understanding of creating music. 
+        <br/><br/>For me, music and design go hand in hand which makes it easy for me to create! Just as much as a musician composes a piece of music to evoke emotions and convey a message, a designer creates visuals or experiences that communicate ideas and engage with people.  I am excited to share my knowledge and skills to create meaningful designs and music! 
+</span>
         </div>
     </section>
 
-    <section id="aboutImage"></section>
+    <section id="aboutImage"><img src="images/Tanay.jpeg" alt="About Section Image"></section>
 
     <section id="aboutValues">
         <div id="valuesTitle" class="valuesList">My Values:</div>
