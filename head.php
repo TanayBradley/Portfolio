a2bc1affc081af6e1e571fa4cb4743413c115453
Tanay Bradley <tanaysbradley@gmail.com>
2025-04-25 20:32:12 -0500

    Creative Update

--- head.php
+++ head.php
@@ -6,5 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="stylesheet.css">
     <script src="https://kit.fontawesome.com/8db937c793.js" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
