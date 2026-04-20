97dcf64a176da330dbf9148a4d77d73299284377
TanayBradley <tanaysbradley@gmail.com>
2026-04-20 11:00:47 -0500

    Test

--- index.php
+++ index.php
@@ -52,6 +52,21 @@
                 <span class="projectTitle">Resolved a purchase order integration issues between Amazon and ERP</span>
                 <span class="projectSubtitle">Diagnosed and fixed data mapping issues preventing automated PO processing</span>
             </div>
+            <div class="projectListing">
+    <div class="projectDesc">
+        <span class="projectTitle">Your fourth case study title goes here</span>
+        <span class="projectSubtitle">Your fourth case study subtitle goes here</span>
+    </div>
+    <div class="projectListing">
+    <div class="projectDesc">
+        <span class="projectTitle">Your fourth case study title goes here</span>
+        <span class="projectSubtitle">Your fourth case study subtitle goes here</span>
+    </div>
+    <div class="projectListing">
+    <div class="projectDesc">
+        <span class="projectTitle">Your fourth case study title goes here</span>
+        <span class="projectSubtitle">Your fourth case study subtitle goes here</span>
+    </div>
         </div>
     </section>
 
