cd873a8b77de0ac80a5cd9e07eea64b33a34c3f9
TanayBradley <tanaysbradley@gmail.com>
2026-04-21 21:07:30 -0500

    uop

---
+++ htaccess-2/KeepGoingWeb.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <!-- Character encoding for proper text rendering -->
+    <meta charset="UTF-8"> 
+
+    <!-- Viewport settings for responsive design -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+
+    <!-- Page title displayed on the browser tab -->
+    <title>Tanay Bradley</title> 
+
+    <link rel="stylesheet" href="stylesheet.css">
+</head>
+
+<?php include "head.php"; ?>
+
+<body>
+    <!--[if lt IE 7]>
+    <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    <?php include "navigation.php"; ?>
+
+    <section class="designListing" id="KeepGoingWeb"> 
+        <div class="designImage">
+            <img src="images/Laptop.png" />
+        </div>
+    </section>
+
+    <section id="underline2" class="designListing">
+        <div class="designTitle">
+                <span>Keep</span>>Going First Aid - <span Id="webdesigncolor"> Web Design</span>
+        </div>
+
+        <div class="designDescSplit">
+            <div>
+                <div class="designDescTitle">Overview:</div>
+                <div class="designDescBodyPrimary">
+                After successfully developing a prototype app for Keep>Going First Aid to educated and attract new customers, the company is now confronted with the challenge of efficiently managing a surge in incoming online orders.
+                    <br/><br/>
+                    Through collaboration with stakeholders and company owners, we have identified a solution to address this challenge for Keep>Going First Aid. 
+                </div>
+            </div>
+            <div>
+                <div class="designDescTitle">Role:</div>
+                <div class="designDescBodyPrimary">
+                    <span>UX/UI Designer and Project Manager</span><br/><br/>
+                    Proposal, Research Findings, Wireframes, Interactive Prototype and Evaluation Results. 
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="underline2" class="designListing">
+        <div class="designTitle">
+            <span>Design Process</span>
+        </div>
+        <div class="designDesc">
+        <div class="designDescBodyThreeCol">
+    <div class="design-item">
+        <img src="images/User Research.png" alt="Description of your design">
+        <p>User Research:</p>
+        <div class="additional-text">   
+            <span>Identify Client Needs</span>
+            <span>Competitive Analysis</span>
+        </div>
+    </div>
+    <div class="design-item">
+        <img src="images/Critique.png" alt="Description of your design">
+        <p>Critique:</p>
+        <div class="additional-text">
+            <span>User Personas</span>
+            <span>Site Maps</span>
+            <span>Design Critique</span>
+        </div>
+    </div>
+    <div class="design-item">
+        <img src="images/Wireframes.png" alt="Description of your design">
+        <p>Wireframes:</p>
+        <div class="additional-text">
+            <span>Sketches</span>
+            <span>Lo-Fidelity Wireframe</span>
+            <span>Hi-Fidelity Wireframe</span>
+        </div>
+    </div>
+    <div class="design-item">
+        <img src="images/Mockups.png" alt="Description of your design">
+        <p>Mockup:</p>
+        <div class="additional-text">
+            <span>Prototype</span>
+        </div>
+    </div>
+    <div class="design-item">
+        <img src="images/Test.png" alt="Description of your design">
+        <p>Test:</p>
+        <div class="additional-text">
+            <span>Feedback</span>
+            <span>Conclusion</span>
+            <span>Future Concept</span>
+        </div>
+    </div>
+</div>
+
+        </div>
+    </section>
+
+    <section id="underline2" class="designListing">
+        <div class="designTitle">
+            <span>Defining The Problem</span>
+        </div>
+        <div class="designDesc">
+            <div class="designDescBodySecondary">
+                Keep > Going First Aid lacks a mobile game to complement their existing website.
+                The gap in their digital presence limits their ability to engage users beyond the website and fully leverage the potential for user interaction and education.
+                To ensure we solve their problem effectively and efficiently, here is what our research will include:
+            </div> <br/><br/>
+        <div class="additional-text">
+    <span id="numbers" class="number">1.</span> Understanding the user and their needs<br><br/>
+    <span id="numbers" class="number">2.</span> Uncovering pain points<br><br/>
+    <span id="numbers" class="number">3.</span> Determining the success of the tasks measured
+</div>
+
+        </div>
+    </section>
+
+    <section id="underline2" class="designListing">
+        <div class="designTitle">
+            <span>Target Audience</span>
+        </div>
+        <div class="designDesc">
+            <div class="designDescBodySecondary">
+            The target audience are the stakeholders who are employed at Keep>Going First Aid such as the Warehouse Operations Manager, Sales Manager, and Customer Service Representatives. 
+
+
+        </div>
+    </section>
+
+    <section id="underline2" class="designListing">
+        <div class="designTitle">
+            <span>User Research</span>
+        </div>
+        <div class="designDesc">
+            <div class="designDescBodySecondary">
+            User Research for the online orders project involved understanding the needs and behaviors of key stakeholders who work at keep>Going First Aid. By understanding the specific requirements, we can tailor the system to meet their needs effectively.   
+            </div> 
+    </section>
+
+    <section id="underline2" class="designListing">
+        <div class="designTitle">
+            <span>User Persona</span>
+            </div> 
+            <br/><br/> 
+            <div class="small-image2">
+    <img src="images/Personas.png" alt="Personas"> 
+</div>
+        </div>
+    </section>
+
+    <section id="underline2" class="designListing">
+        <div class="designTitle">
+            <span>Sitmap/Swimlane</span>
+        </div>
+        <div class="designDesc">
+            <div class="designDescBodySecondary">
+            A sitemap/Swimlane was created to plan and organize the structure of the website.   
+            </div> 
+            <br/><br/> <br/><br/> <img src="images/Swimlane.png" alt="Swimlane">
+        </div>
+    </section>
+
+    <section id="underline2" class="designListing">
+        <div class="designTitle">
+            <span>Sketches</span>
+        </div>
+        <div class="small-images2">
+            <br/><br/> <img src="images/Sketches.png" alt="Sketches">
+        </div>
+    </section>
+
+    <section id="underline2" class="designListing">
+        <div class="designTitle">
+            <span>Hi-Fidelity Wireframes</span>
+        </div>
+        <div class="designDesc">
+            <br/><br/> <img src="images/Final.png" alt="Final">
+        </div>
+    </section>
+
+    <section id="underline2" class="designListing">
+        <div class="designTitle">
+            <span>Key Takeaways</span>
+        </div>
+        <div class="designDesc">
+            <div class="designDescBodySecondary">
+            Through the design process for Keep>Going First Aid, I learned that the true essence of UX design lies in empathizing with users to understand their need. By researching the key stakeholders such as customer service reps, warehouse operations managers, and sales managers, I gained insights that shaped the design of an internal system to manage online orders efficiently. 
+            <br/><br/>
+            This project highlighted the importance of collaboration and iteration in design. As a designer, questions I constantly asked myself were “Why am I putting this here” and “why did I design this that way.” Each stage, from creating user personas to developing hi-fidelity wireframes, was an opportunity to refine the solution iteratively, ensuring that it met the diverse needs
+            of the stakeholders. This experience shaped my belief that impactful design is not just about creating visually appealing interfaces, but about crafting solutions that enhance user experience efficiently and effectively, and driving business objectives.   
+            </div> 
+        </div>
+    </section>
+
+    <hr/>
+
+    <section id="underline2" class="designListing">
+        <div class="designTitle">
+            <span>More Projects</span>
+        </div>
+        <div class="projectListing">
+            <div class="projectImage">
+                <img src="images/Laptop.png" />
+            </div>
+            <div class="projectDesc">
+                <span class="projectTitle">Keep Going > First Aid</span>
+                <span class="projectSubtitle">A backend system for processing online orders</span>
+            </div>
+        </div>
+        <div class="projectListing">
+            <div class="projectImage">
+                <img src="images/SinglePhone.png" />
+            </div>
+            <div class="projectDesc">
+                <span class="projectTitle">Gante</span>
+                <span class="projectSubtitle">An easy way to shop for furniture online</span>
+            </div>
+        </div>
+    </section>
+
+    <?php include "footer.php"; ?>
+</body>
+
+</html>
