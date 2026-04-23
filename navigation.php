debf134241fa9755e2313ff8cc0992e160b2e569
TanayBradley <tanaysbradley@gmail.com>
2026-04-22 21:48:03 -0500

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,46 @@
+
+<div id="navBar">
+    <div id="navBarLeft">
+        <div id="name" class="navOption">Tanay Bradley</div>
+    </div>
+    <div id="navBarRight">
+        <nav id="navMenu">
+            <div class="navOption">
+                <a href="index.php">Home</a>
+            </div>
+            <div class="navOption dropdown-trigger">
+                Designs <i class="fa-solid fa-chevron-down"></i>
+                <ul class="dropdown-menu">
+                    <li><a href="KeepGoingMobile.php">Keep Going Mobile App</a></li>
+                    <li><a href="KeepGoingWeb.php">Keep Going Website</a></li>
+                    <li><a href="GanteMobile.php">Grante Mobile App</a></li>
+                </ul>
+            </div>
+            <div class="navOption">
+                <a href="about.php">About</a>
+            </div>
+        </nav>
+        <div id="contrastMode" class="light-mode" onclick="switchMode()">Light/Dark</div>
+    </div>
+</div>
+
+<script>
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdownTriggers = document.querySelectorAll('.dropdown-trigger');
+    
+    // Loop through each dropdown trigger
+    dropdownTriggers.forEach(trigger => {
+        const dropdownMenu = trigger.querySelector('.dropdown-menu');
+        
+        // Show the dropdown on hover
+        trigger.addEventListener('mouseover', function() {
+            dropdownMenu.style.display = 'block';
+        });
+
+         // Hide the dropdown when the mouse leaves
+        trigger.addEventListener('mouseleave', function() {
+            dropdownMenu.style.display = 'none';
+        });
+    });
+});
+</script>
