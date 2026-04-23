2612f58ccac4a68d0dbcb3ec96f773a5d383babf
TanayBradley <tanaysbradley@gmail.com>
2026-04-23 12:12:15 -0500

    About page update

--- about.php
+++ about.php
@@ -34,7 +34,7 @@
         </div>
 </section>
 
-    <section id="aboutImage"><img src="images/Tanay.jpeg" alt="About Section Image"></section>
+    <section id="aboutImage"><img src="images/Me.jpg" alt="About Section Image"></section>
 
     <section id="aboutValues">
         <div id="valuesTitle" class="valuesList">My Values:</div>
